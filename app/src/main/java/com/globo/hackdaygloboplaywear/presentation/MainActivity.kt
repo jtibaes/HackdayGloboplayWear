3b2dc8f78ac7e9cd9e4cd0d186e0d08bcab28aae
Juliana Tibães <juliana.tibaes@g.globo>
2024-08-21 18:20:38 -0300

    interface

--- app/src/main/java/com/globo/hackdaygloboplaywear/presentation/MainActivity.kt
+++ app/src/main/java/com/globo/hackdaygloboplaywear/presentation/MainActivity.kt
@@ -15,19 +15,30 @@ import android.provider.Settings.Global.getString
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
@@ -85,12 +96,35 @@ fun WearApp(greetingName: String) {
 
 @Composable
 fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp, top = 20.dp),
+            textAlign = TextAlign.Center,
+            color = Color(0xFFDB5103),
+            text = stringResource(R.string.hello_world, greetingName)
+        )
+        Button(
+            modifier = Modifier.padding(top = 5.dp).size(ButtonDefaults.LargeIconSize),
+            onClick = { },
+            
+        ) {
+            Text(text = "GP")
+        }
+        Button(
+            modifier = Modifier.padding(top = 5.dp, bottom = 20.dp),
+            onClick = { },
+            ) {
+            Text(text = "Bets")
+        }
+
+    }
+
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
